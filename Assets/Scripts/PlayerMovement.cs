7d0bc64064ba4db87fc6d55fcce757d5c024104e
VitaL45 <dheerajpatel345@outlook.com>
2025-07-30 17:28:51 +0530

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpeed = 10f;
+    [SerializeField]
+    private float jumpSpeed = 8f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Translate(Vector2.right * horizontalInput * moveSpeed * Time.deltaTime);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        //Flip player
+        if (horizontalInput < -0.01)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (horizontalInput > 0.01)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        //Jump 
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+}
