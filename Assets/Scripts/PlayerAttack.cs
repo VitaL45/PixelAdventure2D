8284a3cc292982b365427584025b4d0ce0101567
VitaL45 <dheerajpatel345@outlook.com>
2025-08-01 21:34:39 +0530

    projectile implmenetd

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] sandballs;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
+            Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+        sandballs[FindSandball()].transform.position = firePoint.position;
+        sandballs[FindSandball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    //Understand it later
+    private int FindSandball()
+    {
+        for (int i = 0; i < sandballs.Length; i++)
+        {
+            if (!sandballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+}
